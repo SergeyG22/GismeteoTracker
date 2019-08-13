380c9d95b9bd0fcb747ed60c12a5b262e3750923
SergeyG <serggavrilov1986@gmail.com>
2019-08-13 12:40:26 +0300

    добавлена реализация для чтения html средствами qnetwork

--- QtParcer/QtParcer.cpp
+++ QtParcer/QtParcer.cpp
@@ -5,32 +5,32 @@ QtParcer::QtParcer(QWidget* parent)
 {
 	ui.setupUi(this);
 	ui.lineEdit->setText("https://www.gismeteo.ru/weather-sankt-peterburg-4079/now/");
-	manager = new QNetworkAccessManager(this);
+	manager = new QNetworkAccessManager(this); // создаем обьект подключения
 }
 
 void QtParcer::on_pushButton_clicked()
 {
-	QUrl url(ui.lineEdit->text());
-	QNetworkRequest request(url);
-	QNetworkReply* reply = manager->get(request);
-	connect(reply,SIGNAL(finished()),this,SLOT(replyFinished()));
+	QUrl url(ui.lineEdit->text());            // получаем URL
+	QNetworkRequest request(url);             // отправляем запрос по URL
+	QNetworkReply* reply = manager->get(request); // получаем ответ
+	connect(reply,SIGNAL(finished()),this,SLOT(replyFinished())); // генерируем слот подключения
 }
 
 void QtParcer::replyFinished()
 {
 	QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
 
-	if (reply->error() == QNetworkReply::NoError)
+	if (reply->error() == QNetworkReply::NoError)                    // если нет ошибок
 	{
-		QByteArray content = reply->readAll();
-		QTextCodec* codec = QTextCodec::codecForName("utf-8");
-		ui.textEdit->setPlainText(codec->toUnicode(content.data()));		
+		QByteArray content = reply->readAll();                       // сохраняем в массив байтов
+		QTextCodec* codec = QTextCodec::codecForName("utf-8");       // преобразуем кодировку
+		ui.textEdit->setPlainText(codec->toUnicode(content.data())); // выводим результат	
 	}
-	else
-	{
-		ui.textEdit->setPlainText(reply->errorString());
+	else                                                            // если ошибка
+	{ 
+		ui.textEdit->setPlainText(reply->errorString());            // то выводим ошибку
 	}
 
 	
-	reply->deleteLater();
+	reply->deleteLater();                                       //удаляем обьект ответа
 }
